d47831327ec681872ff4d0fba9e452802c1dbf6d
ak-suna <anuskagc100@gmail.com>
2025-12-11 14:55:08 +0545

    Enabled dark mode in Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class', // ADD THIS LINE!
   theme: {
     extend: {},
   },
