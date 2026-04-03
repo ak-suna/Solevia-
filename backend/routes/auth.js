110355859132280aa24c1b0078efb1815a0f4790
ak-suna <anuskagc100@gmail.com>
2026-04-03 10:45:04 +0545

    removed unnessary comments

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,4 @@
-// import express from "express";
-// import { loginUser, registerUser, verifyEmail} from "../controllers/userController.js";
 
-// const router = express.Router();
-
-// router.post("/signup", registerUser);
-// router.post("/login", loginUser);
-// router.get("/verify-email/:code", verifyEmail);
-
-// export default router;
 import express from "express";
 import { 
     loginUser, 
